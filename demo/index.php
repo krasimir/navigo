408d497379feffeac8bd6878fb99ee20118f8fa8
Krasimir Tsonev <krasimir@outset.ws>
2016-02-09 06:39:15 +0200

    Correct path to navigo.js

--- demo/index.php
+++ demo/index.php
@@ -132,7 +132,7 @@ this/is/{{language}}/{{what}}</pre>
 
   <a href="https://github.com/krasimir/navigo" target="_blank"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/c6625ac1f3ee0a12250227cf83ce904423abf351/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png"></a>
 
-  <script src="http://krasimir.github.io/navigo/demo/navigo.js"></script>
+  <script src="http://krasimir.github.io/navigo/lib/navigo.js"></script>
   <script src="<?php echo $root; ?>scripts.js"></script>
   <script src="<?php echo $root; ?>vendor/chai.js"></script>
   <script src="<?php echo $root; ?>vendor/mocha.js"></script>
