a9031679331031b72b533409bb2efcf8de0c7b4b
Krasimir Tsonev <krasimir@outset.ws>
2017-11-17 06:57:06 +0200

    Adding a test covering #167

---
+++ test/spec/issues/167.spec.js
@@ -0,0 +1,14 @@
+import Navigo from '../../../lib/navigo';
+
+describe('Given the issue #167', function () {
+  it('should have parameterized routes that work', function () {
+    var handler = sinon.spy();
+    var router = new Navigo('http://site.com/', false);
+
+    router.on('/products/:id', handler);
+
+    router.resolve('/products/421');
+
+    expect(handler).to.be.calledOnce.and.to.be.calledWith({ id: '421' });
+  });
+});
