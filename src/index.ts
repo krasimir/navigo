3dc8a17db19b5ed8b46b4b65c6fc1b07b3395fa1
Matthew John <matthew@dockstudios.co.uk>
2023-08-04 09:16:24 +0100

    Initial draft for handling named regex param group replacement in "generate" method

--- src/index.ts
+++ src/index.ts
@@ -312,7 +312,19 @@ export default function Navigo(appRoute?: string, options?: RouterOptions) {
       if (data) {
         for (let key in data) {
           result = result.replace(":" + key, data[key]);
+
+          // If regex group replacement is enabled, search
+          // for named-regex parameter groups
+          if (options?.replaceRegexGroups === true) {
+            let regexEscapedKey = key.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+            // Note that this won't handle regex groups that contain sub-groups
+            // e.g. /(?<groupName>(:?example)?)
+            let keyRegex = new RegExp(`\(\?\<${regexEscapedKey}\>[^\(^\)]+\)`);
+            result = result.replace(keyRegex, data[key]);
+          }
         }
+
       }
       result = !result.match(/^\//) ? `/${result}` : result;
     }
